59c7a582ed388a3c757c5f854fa7c5b94512af20
Rushikesh Sargar <35379918+RishiSargar@users.noreply.github.com>
2020-09-14 02:45:33 -0500

    Update settings.py

--- slr_backend/slr_backend/settings.py
+++ slr_backend/slr_backend/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_lzp!m7nqi%8dpeahzru4*$kobpn$+a7&vb0=rtkcgp1a6jj0_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
